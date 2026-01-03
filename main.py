0d292a5418194566be4d43bed0f2a571a84aac4e
000eth <110105272+000eth@users.noreply.github.com>
2026-01-03 21:49:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,10 @@ from aiogram import Bot, Dispatcher
 
 from config import settings
 
-# === ROUTERS ===
-from handlers.check import router as check_router
-from handlers.settings import router as settings_router
 from handlers.start import router as start_router
+from handlers.settings import router as settings_router
+from handlers.check import router as check_router
 
-# === AUTO MONITOR ===
 from logic.auto_monitor import auto_monitor_loop
 
 logging.basicConfig(level=logging.INFO)
@@ -19,15 +17,12 @@ async def main():
     bot = Bot(settings.BOT_TOKEN)
     dp = Dispatcher()
 
-    # Подключаем все хендлеры
     dp.include_router(start_router)
     dp.include_router(settings_router)
     dp.include_router(check_router)
 
-    # Запускаем авто-мониторинг
     asyncio.create_task(auto_monitor_loop(bot))
 
-    # Запуск бота
     await dp.start_polling(bot)
 
 
