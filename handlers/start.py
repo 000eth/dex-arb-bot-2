a3fd012329353cd4d95d958fdb5a01fcd5c2b695
000eth <110105272+000eth@users.noreply.github.com>
2026-01-03 21:57:17 +0300

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,14 +1,11 @@
-from aiogram import Router, types
-from aiogram.filters import Command
-
+from aiogram import Router, types, F
 from keyboards import main_menu
 
 router = Router()
 
-
-@router.message(Command("start"))
+@router.message(F.text == "/start")
 async def start_cmd(message: types.Message):
     await message.answer(
-        "Бот запущен. Выбирай действие:",
+        "Добро пожаловать! Выбирай действие:",
         reply_markup=main_menu
     )
